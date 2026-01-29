6c9c9496a1ff0b74bb151bfeaae69a8d541ea242
Angular1337 <gtmrvsmcpd@gmail.com>
2026-01-29 04:40:04 +0300

    modified:   css/base.css 	modified:   index.html 	modified:   js/styles.js

--- js/styles.js
+++ js/styles.js
@@ -8,3 +8,32 @@
     document.head.appendChild(link);
   });
 })();
+
+(function () {
+  const cursor = document.getElementById("cursor");
+  if (!cursor) return;
+
+  let mouseX = window.innerWidth / 2;
+  let mouseY = window.innerHeight / 2;
+  let posX = mouseX;
+  let posY = mouseY;
+
+  const speed = 0.1;
+
+  window.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function animate() {
+    posX += (mouseX - posX) * speed;
+    posY += (mouseY - posY) * speed;
+
+    cursor.style.transform =
+      "translate(" + posX + "px," + posY + "px) translate(-50%, -50%)";
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+})();
